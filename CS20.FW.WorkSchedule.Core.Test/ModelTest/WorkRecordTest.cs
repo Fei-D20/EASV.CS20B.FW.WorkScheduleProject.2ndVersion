77fc4a8c4ae0e99e1932b064c392c6ad73dae2fa
Fei <evensnachi1@gmail.com>
2022-01-25 11:14:47 +0100

    Unit test work record and initial WorkRecord

---
+++ CS20.FW.WorkSchedule.Core.Test/ModelTest/WorkRecordTest.cs
@@ -0,0 +1,81 @@
+using System;
+using CS20.FW.WorkSchedule.Core.Model;
+using Xunit;
+
+namespace CS20.FW.WorkSchedule.Core.Test.ModelTest
+{
+    public class WorkRecordTest
+    {
+        /// <summary>
+        /// This class is testing about the WorkingRecord of employee check in and check out.
+        /// include the Id of record, employee Id, check in date time. check out date time and working hours per day.
+        /// 
+        /// </summary>
+        public class WorkingRecordTest
+        {
+            #region Initial
+
+            [Fact]
+            public void WorkingRecord_CanBeInitial()
+            {
+                var workingRecord = new WorkingRecord();
+                Assert.NotNull(workingRecord);
+            }
+
+            [Fact]
+            public void WorkingRecord_HaveId()
+            {
+                var workingRecord = new WorkingRecord()
+                {
+                    Id = 1
+                };
+                Assert.Equal(1, workingRecord.Id);
+            }
+
+            [Fact]
+            public void WorkingRecord_HaveEmployeeId()
+            {
+                var workingRecord = new WorkingRecord()
+                {
+                    EmployeeId = 1
+                };
+                Assert.Equal(1, workingRecord.EmployeeId);
+            }
+
+            [Fact]
+            public void WorkingRecord_HaveCheckInTime()
+            {
+                var dateTime = DateTime.Now;
+                var workingRecord = new WorkingRecord()
+                {
+                    CheckInTime = dateTime
+                };
+                Assert.Equal(dateTime, workingRecord.CheckInTime);
+            }
+
+            [Fact]
+            public void WorkingRecord_HaveCheckOutTime()
+            {
+                var dateTime = DateTime.Now;
+                var workingRecord = new WorkingRecord()
+                {
+                    CheckOutTime = dateTime
+                };
+                Assert.Equal(dateTime, workingRecord.CheckOutTime);
+            }
+
+            [Fact]
+            public void WorkingRecord_HaveWorkingHours()
+            {
+                var timeSpan = new TimeSpan(5, 0, 0);
+                var workingRecord = new WorkingRecord()
+                {
+                    WorkingHours = timeSpan
+                };
+                Assert.Equal(timeSpan, workingRecord.WorkingHours);
+            }
+
+            #endregion
+        }
+    }
+}
