999d33d0910b5497c6e597e5e64f9092f5d01fe0
Fei <evensnachi1@gmail.com>
2022-01-26 18:58:35 +0100

    example

--- CS20.FW.WorkSchedule.Domain.Test/ServiceTest/UserServiceTest.cs
+++ CS20.FW.WorkSchedule.Domain.Test/ServiceTest/UserServiceTest.cs
@@ -150,7 +150,7 @@ namespace CS20.FW.WorkSchedule.Domain.Test.ServiceTest
             // Arrange
             var user = new User()
             {
-                Name = "feiHappy..",
+                Name = "feiNotHappy..",
                 Password = password,
                 Role = Role.Employee
             };
